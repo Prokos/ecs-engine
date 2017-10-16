0432f45c1f866983daa32dfd1225dcf69ed88ad9
Kasper Mol <awsumthx@gmail.com>
2017-10-16 11:04:30 +0200

    more comments

--- src/setup.ts
+++ src/setup.ts
@@ -6,6 +6,8 @@ interface Array<T> {
     find(type:any): any[];
 }
 
+// @TODO: how necessary is this?
+// if we use this in the core of our engine, it's not likely to be very performant
 Array.prototype.find = function(type:any) {
 	return this.filter((el:any) => el instanceof type);
 };
